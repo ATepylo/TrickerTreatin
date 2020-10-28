56637346c9aa350f1a6fd7c16f5c56c2edfd3000
ATepylo <45440409+ATepylo@users.noreply.github.com>
2020-10-27 21:16:14 -0400

    updated project to new unity version and started main room script

---
+++ Assets/Scripts/MainRoom.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainRoom : MonoBehaviour
+{
+    public enum GameState { start, knock, wait, pause, end }
+    public GameState currentState = GameState.start;
+
+    [SerializeField]
+    private int eggs;
+
+    public List<GameObject> miniGames;
+
+    //for knock state
+    [SerializeField]
+    private float knockTimer;
+    [SerializeField]
+    private float knockRandom;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(currentState)
+        {
+            case GameState.start://used 
+                if (Input.GetButtonDown("Jump"))
+                {
+                    knockRandom = Random.Range(3, 5);
+                    currentState = GameState.knock;
+                }
+                break;
+            case GameState.knock:
+                knockTimer += Time.deltaTime;
+                if(knockTimer >= knockRandom)
+                {
+                    //play knock sound
+                    currentState = GameState.wait;
+                }
+                break;
+            case GameState.wait:
+
+                break;
+            case GameState.pause:
+                break;
+            case GameState.end:
+                break;
+        }
+
+        if(eggs >= 3)
+        {
+            currentState = GameState.end;
+        }
+    }
+
+    public void EggHouse()
+    {
+        eggs++;
+    }
+}
